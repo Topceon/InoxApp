c4552967dbf1ce6b1d81e9f46a8460323c32fb4e
Inox Danil <inoxdanil@gmail.com>
2023-09-12 12:55:22 +0600

    fixed env vars

--- InoxApp/settings.py
+++ InoxApp/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
+from os.path import join, dirname
 #
-# load_dotenv()
+dotenv_path = join(dirname(__file__), '.env.dev')
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+print(os.getenv("SECRET_KEY"))
+print(os.getenv("DEBUG"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False).lower() in ('true', '1', 't')
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
 
