c3fdae8f88980cb4c650e9573c3d9fa63c344bb1
Inox Danil <inoxdanil@gmail.com>
2023-09-12 13:07:00 +0600

    testing docker env

--- InoxApp/settings.py
+++ InoxApp/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from os.path import join, dirname
+# from os.path import join, dirname
 #
-dotenv_path = join(dirname(__file__), '.env.dev')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), '.env.dev')
+# load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
