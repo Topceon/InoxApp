ee9585eee4e3995f43be97364afb2a7036299771
Inox Danil <inoxdanil@gmail.com>
2023-09-12 08:26:38 +0600

    test environment

--- InoxApp/settings.py
+++ InoxApp/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-va6lb$gb4^omwh$hcepkb_it69e!bp+0g4bywe!gslmrlrf$3%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv("TEST")
 
 ALLOWED_HOSTS = ['*']
 
