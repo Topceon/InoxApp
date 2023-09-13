4d6d6ee4501732ab809429e0d07fa42b990d76f9
Inox Danil <inoxdanil@gmail.com>
2023-09-13 15:40:51 +0600

    Updated static files location

--- InoxApp/settings.py
+++ InoxApp/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR.parent, "staticfiles")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR.parent, "staticfiles"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR.parent, "staticfiles"),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
