98c04e2f3c7edcc1e51b146b5238f0217d1c1e2a
Inox Danil <inoxdanil@gmail.com>
2023-09-13 15:38:59 +0600

    Updated static files location

--- InoxApp/settings.py
+++ InoxApp/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.parent / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR.parent, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR.parent, "staticfiles"),
 ]
