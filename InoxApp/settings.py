5f1c10244eecd7ae15d185c176274025b0b6bea4
Inox Danil <inoxdanil@gmail.com>
2023-09-13 15:35:08 +0600

    Updated static files location

--- InoxApp/settings.py
+++ InoxApp/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR.parent, "static"),
+    os.path.join(BASE_DIR.parent, "staticfiles"),
 ]
 
 # Default primary key field type
