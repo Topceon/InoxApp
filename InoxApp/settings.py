8028cd3e1df41daa2d876bf33caa471f54276b87
Inox Danil <inoxdanil@gmail.com>
2023-09-13 15:11:19 +0600

    Static files location

--- InoxApp/settings.py
+++ InoxApp/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 from os.path import join, dirname
 #
-#Works as intended
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
